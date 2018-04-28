23c3e929531a7ec003fdf2211cd90f3b2082cb09
LAPTOP-9K153T17\Redwan <ahmed.redwan7331@gmail.com>
2018-04-28 15:59:39 -0400

    ch

--- src/utils/WebDriverFuncrions.java
+++ src/utils/WebDriverFuncrions.java
@@ -2,8 +2,12 @@ package utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.log4testng.Logger;
 
 public class WebDriverFuncrions {
+	
+	public static Logger APPLICATION_LOGS = Logger.getLogger("devpinoyLogger");
+	
 	public WebDriver driver ;
 	
 	public void typeByXpath(String loc, String value){
