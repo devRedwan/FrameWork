4eb088deabfecd832cceafc17ab3f0a38ad5bce0
LAPTOP-9K153T17\Redwan <ahmed.redwan7331@gmail.com>
2018-04-29 13:15:22 -0400

    Crossbowsers.testing

--- src/base/config.java
+++ src/base/config.java
@@ -1,10 +1,12 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+
 
 import utils.WebDriverFuncrions;
 
@@ -14,17 +16,33 @@ public class config extends WebDriverFuncrions {
 	
 	
 	
+	String browsertype = "ch";
+	
 	@BeforeTest
 	
-public void initDrivers(){
+public void initDrivers() throws InterruptedException{
 	
 
-	 
+	 if (browsertype.equalsIgnoreCase("FF")){
 	System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\Drivers\\geckodriver.exe");
 	driver = new FirefoxDriver();
-	driver.get("http://www.facebook.com");
+	APPLICATION_LOGS.debug("FIREFOX BROWSER STARTED");
+	
+	
+    } else if (browsertype.equalsIgnoreCase("ch")){
+    	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+    	driver = new ChromeDriver();
+    	APPLICATION_LOGS.debug("CHROME BROWSER STARTED");
+    	
+    	
+    }
+	 driver.get("http://www.facebook.com");
+	 driver.manage().window().maximize();
+	 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS); //implicit wait - will wait for all elements to load
+	 
+  }	
 
-	
-	
-}
+
+
+
 }
